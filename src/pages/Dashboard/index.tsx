5e1222f8611420f796e12f554a12744835f6688e
Maurício A. Baú <mauricio.a.bau@gmail.com>
2020-05-11 20:02:41 -0300

    Utilizando Styled Components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { Title } from './styles'
+
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  return <Title>Eplore repositórios no Guithub</Title>
 }
 
 export default Dashboard;
